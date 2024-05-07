f52bdf39b2c3beb0514e6e1d5dc42b1d7be0d4df
DMaroe <105966834+DMaroe@users.noreply.github.com>
2024-05-07 23:46:07 +0700

    Experiment 3.1: Original code

--- README.md
+++ README.md
@@ -3,15 +3,8 @@
 ### Original code
 
 #### First run
-![3.1.1](images/3.1.1.png)
+![alt text](images/3.1.png)
 
-#### Final code
-![3.1.2](images/3.1.2.png)
 
 ### Add some creativities
 
-![3.2.1](images/3.2.1.png)
-
-![3.2.2](images/3.2.2.png)
-
-Saya mengubah tema warna dari halaman chat dan login serta menambahkan icon profile.
--- src/components/chat.rs
+++ src/components/chat.rs
@@ -2,9 +2,9 @@ use serde::{Deserialize, Serialize};
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
 use yew_agent::{Bridge, Bridged};
-use crate::services::event_bus::EventBus;
 
-use crate::{User, services::websocket::WebsocketService};
+use crate::services::event_bus::EventBus;
+use crate::{services::websocket::WebsocketService, User};
 
 pub enum Msg {
     HandleMsg(String),
@@ -42,9 +42,9 @@ struct UserProfile {
 pub struct Chat {
     users: Vec<UserProfile>,
     chat_input: NodeRef,
+    _producer: Box<dyn Bridge<EventBus>>,
     wss: WebsocketService,
     messages: Vec<MessageData>,
-    _producer: Box<dyn Bridge<EventBus>>,
 }
 impl Component for Chat {
     type Message = Msg;
@@ -93,7 +93,8 @@ impl Component for Chat {
                             .map(|u| UserProfile {
                                 name: u.into(),
                                 avatar: format!(
-                                    "https://api.dicebear.com/8.x/adventurer-neutral/svg?eyebrows=variant10&eyes=variant05&mouth=variant02&backgroundColor=ffd5dc"
+                                    "https://avatars.dicebear.com/api/adventurer-neutral/{}.svg",
+                                    u
                                 )
                                 .into(),
                             })
@@ -114,7 +115,6 @@ impl Component for Chat {
             Msg::SubmitMessage => {
                 let input = self.chat_input.cast::<HtmlInputElement>();
                 if let Some(input) = input {
-                    //log::debug!("got input: {:?}", input.value());
                     let message = WebSocketMessage {
                         message_type: MsgTypes::Message,
                         data: Some(input.value()),
@@ -137,9 +137,10 @@ impl Component for Chat {
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let submit = ctx.link().callback(|_| Msg::SubmitMessage);
+
         html! {
             <div class="flex w-screen">
-                <div class="flex-none w-56 h-screen bg-pink-200">
+                <div class="flex-none w-56 h-screen bg-gray-100">
                     <div class="text-xl p-3">{"Users"}</div>
                     {
                         self.users.clone().iter().map(|u| {
@@ -162,8 +163,8 @@ impl Component for Chat {
                     }
                 </div>
                 <div class="grow h-screen flex flex-col">
-                    <div class="w-full h-14 border-b-2 border-gray-300 bg-pink-100"><div class="text-xl p-3">{"🩷 Chat!"}</div></div>
-                    <div class="w-full grow overflow-auto border-b-2 border-gray-300 bg-pink-100">
+                    <div class="w-full h-14 border-b-2 border-gray-300"><div class="text-xl p-3">{"💬 Chat!"}</div></div>
+                    <div class="w-full grow overflow-auto border-b-2 border-gray-300">
                         {
                             self.messages.iter().map(|m| {
                                 let user = self.users.iter().find(|u| u.name == m.from).unwrap();
@@ -188,10 +189,10 @@ impl Component for Chat {
                         }
 
                     </div>
-                    <div class="w-full h-14 flex px-3 items-center bg-pink-100">
+                    <div class="w-full h-14 flex px-3 items-center">
                         <input ref={self.chat_input.clone()} type="text" placeholder="Message" class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700" name="message" required=true />
-                        <button onclick={submit} class="p-3 shadow-sm bg-pink-500 w-10 h-10 rounded-full flex justify-center items-center color-white">
-                            <svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-white">
+                        <button onclick={submit} class="p-3 shadow-sm bg-blue-600 w-10 h-10 rounded-full flex justify-center items-center color-white">
+                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-white">
                                 <path d="M0 0h24v24H0z" fill="none"></path><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
                             </svg>
                         </button>
